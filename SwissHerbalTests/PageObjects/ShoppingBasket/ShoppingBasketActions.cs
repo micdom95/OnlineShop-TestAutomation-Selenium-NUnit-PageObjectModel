74a6b3a77fa2d22e5f3a8584d3b710b136f3ab99
micdom95 <micdom95@gmail.com>
2021-09-07 16:12:30 +0200

    New Test Cases

---
+++ SwissHerbalTests/PageObjects/ShoppingBasket/ShoppingBasketActions.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwissHerbalTests.PageObjects.ShoppingBasket
+{
+    public class ShoppingBasketActions : ShoppingBasketLocators
+    {
+        private IWebDriver _driver;
+
+        public ShoppingBasketActions(IWebDriver driver) : base(driver)
+        {
+            _driver = driver;
+        }
+    }
+}
---
+++ SwissHerbalTests/PageObjects/ShoppingBasket/ShoppingBasketLocators.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwissHerbalTests.PageObjects.ShoppingBasket
+{
+    public class ShoppingBasketLocators
+    {
+        private IWebDriver _driver;
+
+        public ShoppingBasketLocators(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+    }
+}
